0f084b96c04c9d7be03a403c33f7bc1b4b534705
Volya2301 <161381998+Volya2301@users.noreply.github.com>
2024-06-22 23:14:02 +0200

    Update homework_4.js

--- homework_4.js
+++ homework_4.js
@@ -2,7 +2,7 @@ let checkProbabilityTheory = function (count){
     let genNumbers = [];
     let numb = 1
     while (numb <= count){
-        let randomNum = Math.floor(Math.random() * 1000) + 100
+        let randomNum = Math.floor(Math.random() * 1001) + 100
         genNumbers.push(randomNum)
         numb++
     }
